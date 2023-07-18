f3bec2c8e1661bf76922f7a3f0a7556930cc720c
jabrahanba <jabrahanba@gmail.com>
2023-07-18 17:49:00 -0500

    28

--- pages/05_🙁_Reclamos_del_Servicio.py
+++ pages/05_🙁_Reclamos_del_Servicio.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Setup web page
 st.set_page_config(
@@ -8,3 +9,15 @@ st.set_page_config(
 
 st.title('🙁Reclamos del Servicio')
 st.markdown('***')
+
+total_tipo = pd.read_excel('DENUN-Y-RECLA-12878.xlsx', sheet_name='Total por Tipo')
+st.markdown('### Total de Reclamos por tipo, enero a mayo 2023')
+st.dataframe(total_tipo)
+
+reclamo_servicio = pd.read_excel('DENUN-Y-RECLA-12878.xlsx', sheet_name='Reclamos_servicio')
+st.markdown('### Cantidad de Reclamos por Servicios, enero a mayo 2023')
+st.dataframe(reclamo_servicio)
+
+internet = pd.read_excel('DENUN-Y-RECLA-12878.xlsx', sheet_name='Cinco_internet')
+st.markdown('### Reclamos por prestador de servicio de Internet, enero a mayo 2023')
+st.dataframe(internet)
