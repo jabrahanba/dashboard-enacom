81600a71b68b93a5e0af17e3f8a7d21eeaada9e0
jabrahanba <jabrahanba@gmail.com>
2023-07-18 17:42:18 -0500

    27

---
+++ pages/05_🙁_Reclamos_del_Servicio.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Setup web page
+st.set_page_config(
+     page_title="🙁Reclamos del Servicio",
+     page_icon= "internet.png",
+     layout="wide")
+
+st.title('🙁Reclamos del Servicio')
+st.markdown('***')
---
+++ pages/06_🎯_KPI.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Setup web page
+st.set_page_config(
+     page_title="🎯KPI",
+     page_icon= "internet.png",
+     layout="wide")
+
+st.title('🎯KPI')
+st.markdown('***')
