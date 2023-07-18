eb5563baae8856ba6764e335114f89e9a2690e80
jabrahanba <jabrahanba@gmail.com>
2023-07-18 17:15:03 -0500

    23

--- pages/01_🌍_Contexto_Global.py
+++ pages/01_🌍_Contexto_Global.py
@@ -19,5 +19,5 @@ st.title('🌍Contexto Global')
 st.markdown('***')
 st.markdown("La siguiente información fue extraida del informe 'El estado de digitalización de América Latina frente a la pandemia del COVID 19, disponible en: https://scioteca.caf.com/bitstream/handle/123456789/1540/El_estado_de_la_digitalizacion_de_America_Latina_frente_a_la_pandemia_del_COVID-19.pdf?sequence=1&isAllowed=y ")
 st.markdown("## Estado de la digitalización en América Latina")
-st.image("imagen1.jpg", caption="Digitalización Global", use_column_width=True)
+st.image("imagen1.jpg", caption="Digitalización Global", use_column_width=True, output_format="auto")
     
