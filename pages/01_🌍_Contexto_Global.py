7684892fa0553b9bb5fc36a4b397593c0aea7ca1
jabrahanba <jabrahanba@gmail.com>
2023-07-18 17:08:54 -0500

    20

--- pages/01_🌍_Contexto_Global.py
+++ pages/01_🌍_Contexto_Global.py
@@ -15,9 +15,9 @@ st.set_page_config(
      layout="wide")
 
 st.title('🌍Contexto Global')
-st.markdown('A continuación se presentan los gráficos relativos al acceso a internet a nivel global en Argentina')
-
 
 st.markdown('***')
 st.markdown("La siguiente información fue extraida del informe 'El estado de digitalización de América Latina frente a la pandemia del COVID 19, disponible en: https://scioteca.caf.com/bitstream/handle/123456789/1540/El_estado_de_la_digitalizacion_de_America_Latina_frente_a_la_pandemia_del_COVID-19.pdf?sequence=1&isAllowed=y ")
 st.markdown("## Estado de la digitalización en América Latina")
+st.image("imagen1.jpg", caption="Digitalización Global", use_column_width=True)
+    
