bf0075f2c357e048bc03cb1d00f5558632f29a7f
jabrahanba <jabrahanba@gmail.com>
2023-07-18 17:19:05 -0500

    24

--- pages/01_🌍_Contexto_Global.py
+++ pages/01_🌍_Contexto_Global.py
@@ -1,12 +1,6 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-pd.set_option('display.max_columns', 200) #Mostrar todas las columnas
-pd.set_option('display.float_format', '{:.2f}'.format) #No mostrar notación científica
-import plotly.io as pio
+import os
+image_path1 = os.path.join(os.getcwd(), "imagen1.jpg")
 
 # Setup web page
 st.set_page_config(
@@ -19,5 +13,5 @@ st.title('🌍Contexto Global')
 st.markdown('***')
 st.markdown("La siguiente información fue extraida del informe 'El estado de digitalización de América Latina frente a la pandemia del COVID 19, disponible en: https://scioteca.caf.com/bitstream/handle/123456789/1540/El_estado_de_la_digitalizacion_de_America_Latina_frente_a_la_pandemia_del_COVID-19.pdf?sequence=1&isAllowed=y ")
 st.markdown("## Estado de la digitalización en América Latina")
-st.image("imagen1.jpg", caption="Digitalización Global", use_column_width=True, output_format="auto")
+st.image(image_path1, caption="Digitalización Global", use_column_width=True)
     
